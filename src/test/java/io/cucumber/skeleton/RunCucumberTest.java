5bdc0fcf176f4b40dba177a3a5f523792c5cb3b0
techcybernetics <31638501+techcybernetics@users.noreply.github.com>
2020-12-23 17:10:05 -0500

    Update RunCucumberTest.java

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions( features = "src/test/resources/io/cucumber/skeleton/features/services",
-        monochrome = true, plugin = {"pretty","html:target/HtmlReports.html","json:target/JSONReports/report.json","json:target/JunitReports/report.xml"},tags ="@api",
+        monochrome = true, plugin = {"pretty","html:target/cucumber-report-html","json:target/JSONReports/cucumber.json","json:target/JunitReports/report.xml"},tags ="@api",
         publish = true)
 public class RunCucumberTest {
 }
