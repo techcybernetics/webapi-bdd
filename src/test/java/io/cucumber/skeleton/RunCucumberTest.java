c81aad6c84a064f482ccfa26bca3c7c69e5ed9b4
techcybernetics <31638501+techcybernetics@users.noreply.github.com>
2020-12-23 16:50:13 -0500

    Update RunCucumberTest.java

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -8,11 +8,5 @@ import org.junit.runner.RunWith;
 @CucumberOptions( features = "src/test/resources/io/cucumber/skeleton/features/services",
         monochrome = true, plugin = {"pretty","html:target/HtmlReports.html","json:target/JSONReports/report.json","json:target/JunitReports/report.xml"},tags ="@api",
         publish = true)
-//tags ="@products",
 public class RunCucumberTest {
 }
-
-/*
-to run multiple tags
-mvn clean verify -Dcucumber.filter.tags="@products or @login"
- */
